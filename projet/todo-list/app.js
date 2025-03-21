dc0c73c07ba13fab8ee4a54b8af0fc2408083b6b
jordanetalus <jordanefossitalla@gmail.com>
2025-03-21 06:00:54 +0100

    fin de la todolist

--- projet/todo-list/app.js
+++ projet/todo-list/app.js
@@ -1,23 +1,58 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const port = 3000;
-const tasks = [
-    {
-        title: "apprendre à programmer",
-        done: false,
-    },
-    {
-        title: "faire des courses",
-        done: true,
-    },
+// const tasks = [
+//     {
+//         title: "apprendre à programmer",
+//         done: false,
+//     },
+//     {
+//         title: "faire des courses",
+//         done: true,
+//     },
 
-]
+// ]
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}));
 
 app.set('view engine', 'ejs');
 
+app.post('/task', (req,res) => {
+    if(req.body){
+        req.session.tasks.push({
+            title: req.body.task,
+            done:false,
+        });
+    }
+    res.redirect('/');
+});
+
+app.get('/task/:id/done', (req,res) => {
+    if (req.session.tasks[req.params.id]){
+        req.session.tasks[req.params.id].done = true;
+    }
+    res.redirect('/')
+});
+
+app.get('/task/:id/delete', (req,res) => {
+    if (req.session.tasks[req.params.id]){
+        req.session.tasks.splice(req.params.id,1);
+    }
+    res.redirect('/')
+});
+
 app.get('/', (req,res) => {
-    res.render('todolist',{tasks: tasks});
+    if(!req.session.tasks){
+        req.session.tasks = [];
+    }
+    res.render('todolist',{tasks: req.session.tasks});
 });
 
 app.listen(port, ()=> {
