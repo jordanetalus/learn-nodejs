5348196c669df26ee24ed94ba787ea6d6d9473c7
jordanetalus <jordanefossitalla@gmail.com>
2025-03-25 16:20:42 +0000

    crud avec mongoose

--- projet/mongoose/index.js
+++ projet/mongoose/index.js
@@ -3,19 +3,29 @@ const User = require('./models/User');
 
 
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1/exemple-mongoose');
+    await mongoose.connect('mongodb://127.0.0.1/exemple-mongoose');// connexion à la bd
 
     console.log('connexion ok');
 
-    const user1 = new User({
-        email: "jordane@exemple.com",
-        firstname: "talla",
-        lastname: "jordane",
-        age: 38
+    const users = await User.find();// récupération des documents
+    console.log(users);
+
+    const jordane = await User.findById('67e2ba8d5bc3a1fd931afc77');// récupération spécifique via l'id
+    console.log(jordane);
+
+    const id ='67e2ba8d5bc3a1fd931afc77'; // update d'un document
+    await User.findByIdAndUpdate(id,{
+        email: "jordanefossitalla@gmail.com",
     });
 
-    console.log(user1);
-    await user1.save();
-}
+    const rest = await User.updateMany(// update de plusieurs documents 
+        {lastname:'jordane'},
+        {firstname:'fossi talla'},
+    );
 
+    console.log(rest);
+
+    mongoose.disconnect();
+}
+    
 main();
